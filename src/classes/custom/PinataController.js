64197ab040b9ad2bc5b2acc1cce921b20e83c905
Dan Koenig <dan.koenig@helloworld.com>
2022-09-09 22:27:05 -0400

    add pinata model, simulate rope

--- src/classes/custom/PinataController.js
+++ src/classes/custom/PinataController.js
@@ -1,70 +1,88 @@
-import BaseController from '../controllers/BaseController';
+import CustomMeshController from '../controllers/CustomMeshController';
 import { basicColorMaterial } from '../../utils/THREEHelpers';
-export default class PinataController extends BaseController{
+import * as THREE from 'three';
+import { degreesToRadians } from '../../utils/Utilities';
+export default class PinataController extends CustomMeshController{
     constructor(data){
-        super(data);
+        super(data, 'https://danielpatrickkoenig.github.io/shared-app-resources/pinata3.glb');
 
+        // const basePositions = this.getBasePositions();
+
+        this.onPinataMoved = null;
+        this.links = [];
+        this.pipes = [];
+        this.pinata = null;
+    }
+    modelLoaded(model){
+        model.scale.z = -1;
+        model.scale.x = -1;
         const redMat = basicColorMaterial('cc0000');
         const chainProps = { start: {x: 0, y: 16, z: 0}, jump: 1.125 };
         const chainTop = this.environment.createSphere({size: {r: .2}, position: chainProps.start, material: redMat, mass: 0 });
-        const chainObjects = [...new Array(6).keys()].map(item => this.environment.createSphere({size: { r: .2 }, position: { ...chainProps.start, y: chainProps.start.y - (chainProps.jump * (item + 1)) }, material: redMat, mass: 1 }));
-        chainObjects.forEach((item, index) => {
-            const lastObj = index === 0 ? chainTop.body : chainObjects[index - 1].body;
+        this.links = [...new Array(6).keys()].map(item => this.environment.createSphere({size: { r: .2 }, position: { ...chainProps.start, y: chainProps.start.y - (chainProps.jump * (item + 1)) }, material: redMat, mass: 1 }));
+        // this.links = [...new Array(6).keys()].map(item => this.environment.createSphere({size: { r: .2 }, position: basePositions.links[item], material: redMat, mass: 1 }));
+        this.links.forEach((item, index) => {
+            const lastObj = index === 0 ? chainTop.body : this.links[index - 1].body;
             this.environment.physics.createHinge(lastObj, item.body, .5);
 
         });
 
-        this.pinata = this.environment.createBox({size: {x: 2, y: 1, z: 3}, position: { ...chainProps.start, y: chainProps.start.y - (chainProps.jump * (chainObjects.length)) - (chainProps.jump * 2) }, material: redMat, mass: 20 });
-        this.environment.physics.createHinge(chainObjects[chainObjects.length - 1].body, this.pinata.body, 1);
-        
-        /*
-        setTimeout(() => {
-            const forceBase = 5000;
-            const force = {
-                x: ((Math.random() * forceBase) + forceBase),
-                y: (Math.random() * forceBase) + (forceBase / 2),
-                z: (Math.random() * (forceBase / 2)) + (forceBase / 5)
-            };
-            this.environment.physics.addForce(this.pinata.body, force);
-            console.log('hit');
-        }, 10000);
+        this.pinata = this.environment.createBox({size: {x: 2, y: 1, z: 3}, position: { ...chainProps.start, y: chainProps.start.y - (chainProps.jump * (this.links.length)) - (chainProps.jump * 2) }, customMesh: model, mass: 20 });
+        // this.pinata = this.environment.createBox({size: {x: 2, y: 1, z: 3}, position: basePositions.pinata, material: redMat, mass: 20 });
+        this.environment.physics.createHinge(this.links[this.links.length - 1].body, this.pinata.body, 1);
 
-        const forceBase = 2500;
-        setTimeout(() => {
+        this.pipes = this.links.map((item, index) => {
+            const container = new THREE.Object3D();
+            const innerContainer = new THREE.Object3D();
+            const blueMat = basicColorMaterial('0000cc');
+            const pipe = this.environment.createCylinder({ size: {r: .1, y: 1.5}, position: { x: 0, y: 0, z: 0 }, material: blueMat })
             
-            const force = {
-                x: ((Math.random() * forceBase) + forceBase) * -1,
-                y: (Math.random() * forceBase) + (forceBase / 2),
-                z: (Math.random() * (forceBase / 2)) + (forceBase / 5)
-            };
-            this.environment.physics.addForce(this.pinata.body, force);
-            console.log('hit');
-        }, 13000);
+            this.links[index].mesh.add(container);
+            container.add(innerContainer);
+            innerContainer.add(pipe.mesh);
+            innerContainer.rotation.x = degreesToRadians(90);
+            innerContainer.position.z = .75 
 
-        setTimeout(() => {
-            const force = {
-                x: ((Math.random() * forceBase) + forceBase),
-                y: (Math.random() * forceBase) + (forceBase / 2),
-                z: (Math.random() * (forceBase / 2)) + (forceBase / 5)
-            };
-            this.environment.physics.addForce(this.pinata.body, force);
-            console.log('hit');
-        }, 16000);
-        setTimeout(() => {
-            const force = {
-                x: ((Math.random() * forceBase) + forceBase) * -1,
-                y: (Math.random() * forceBase) + (forceBase / 2),
-                z: (Math.random() * (forceBase / 2)) + (forceBase / 5)
-            };
-            this.environment.physics.addForce(this.pinata.body, force);
-            console.log('hit');
-        }, 19000);
-        // */
+            // container.rotation.x = degreesToRadians(90);
+            // container.rotation.z = degreesToRadians(90);
+            // container.rotation.y = degreesToRadians(90);
+            const start = item.mesh;
+            const end = index === this.links.length - 1 ? this.pinata.group : this.links[index + 1].mesh;
+            return {start, end, container};
+
+        });
+        
+        
     }
     update(){
-        if(this.pinata){
-            console.log(this.pinata.mesh.rotation);
+        // console.log({ p: this.pinata, l: this.links });
+        if(this.pinata && this.onPinataMoved){
+            this.onPinataMoved({ position: this.pinata.mesh.position, rotation: this.pinata.mesh.rotation });
         }
+        this.pipes.forEach((item, index) => {
+            if(item.end && item.end.position && index < this.pipes.length - 1){
+                item.container.lookAt(item.end.position);
+            }
+            
+        });
+        // for(let i = 0; i < this.links.length; i++){
+        //     this.pipes[i].pipe.position.x = this.links[i].mesh.position.x;
+        //     this.pipes[i].pipe.position.y = this.links[i].mesh.position.y;
+        //     this.pipes[i].pipe.position.z = this.links[i].mesh.position.z;
+        // }
+        // if(this.links.length && this.pipes.length){
+        //     this.pipes.forEach((item, index) => {
+        //         console.log(item);
+                
+        //     });
+        // }
         
+    }
+
+    getBasePositions () {
+        return {
+            links: JSON.parse('[{"x":0.25000260531054147,"y":15.279178231653827,"z":0.2496779888851987},{"x":0.2500080232775214,"y":13.851506029054319,"z":0.24902614736459736},{"x":0.2500140677826064,"y":12.424187436325294,"z":0.2483506005567428},{"x":0.25002093127805625,"y":10.99730527686605,"z":0.24765474425916392},{"x":0.25002857564798175,"y":9.570933042333886,"z":0.24694369934367674},{"x":0.2500365156923629,"y":8.145140711104311,"z":0.246222896756961}]'),
+            pinata: JSON.parse('{"x":0.24926154920356008,"y":5.316430174930662,"z":0.2621179257249421}')
+        }
     }
 }
--- src/classes/custom/PinataScene.js
+++ src/classes/custom/PinataScene.js
@@ -1,21 +1,38 @@
-import { basicColorMaterial } from '../../utils/THREEHelpers';
 import BaseScene from '../BaseScene';
 import LightController, { LightTypes } from '../../classes/controllers/LightController';
 import PinataController from './PinataController';
+import { degreesToRadians } from '../../utils/Utilities';
+import { basicColorMaterial } from '../../utils/THREEHelpers';
 export default class PinataScene extends BaseScene{
     constructor(el){
         super(el);
-        this.pinata = null;
+        this.pinataController = null;
     }
     initialize(){
         const lc = new LightController({ environment: this.environment });
-        lc.addLight({ type: LightTypes.DIRECTIONAL, intensity: 4, target: { x: 0, y: -10, z: -5 } });
+        lc.addLight({ type: LightTypes.DIRECTIONAL, intensity: 4, target: { x: 0, y: -10, z: 5 } });
 
         this.environment.cameraContainer.position.y = 6;
-        this.environment.cameraContainer.position.z = 20;
+        this.environment.cameraContainer.position.z = -20;
+        this.environment.cameraContainer.rotation.y = degreesToRadians(180);
 
-        new PinataController({ environment: this.environment });
+        this.pinataController = new PinataController({ environment: this.environment });
 
+        this.pinataController.onPinataMoved = (data) => {
+            this.emitActionHandler(data, 'automatic');
+        };
+
+    }
+    async hitPinata(){
+        const hitter = this.environment.createSphere({size: {r: .25}, position: {x: this.pinataController.pinata.mesh.position.x, y: this.pinataController.pinata.mesh.position.y, z: this.pinataController.pinata.mesh.position.z - 3},mass: 5, material: basicColorMaterial('cc00cc')});
+        this.environment.physics.addForce(hitter.body, {x: 0, y: 0, z: 12000});
+        hitter.mesh.visible = false;
+
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        this.environment.physics.world.remove(hitter.body);
+        this.environment.scene.remove(hitter.mesh);
+
+        // this.environment.physics.addForce(this.pinataController.pinata.body, {x: (Math.random() * 4000) - 2000, y: Math.random() * 4000, z: 6000});
     }
     
 }
